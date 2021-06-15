045ba1725dbef50ea78c24a6d671131d6c62dd7d
YuriyNaydukh <yurasha2804@gmail.com>
2021-06-15 23:29:25 +0300

    HW3.py

---
+++ lesson1/Цезар і калькулятор.py
@@ -0,0 +1,22 @@
+# a = int(input("1: "))
+# b = int(input("2: "))
+# # c = input("sines: ")
+# c = a+b
+# if a + b == c:
+#     print(c)
+# if c == "+":
+#     print(a+b)
+# elif c == "-":
+#     print(a-b)
+# elif c == "*":
+#     print(a*b)
+# elif c == "/" and b != 0:
+#     print(a/b)
+# elif c == "**":
+#     print(a**b)
+# elif c == "//":
+#     print(a//b)
+
+my_list = 'hhhhhhhhhhhhhhhhjjjjjjjjjjjkkkkkkkk, jjuihuh'
+for i, v in enumerate(my_list, 1):
+    print(i, v)
